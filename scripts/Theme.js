3f2ff4d755bfc90de5510ad55772e3cf111e5691
Sofya <48826470+SofyaPim@users.noreply.github.com>
2020-10-28 13:29:09 +0300

    rewrite theme.js

--- scripts/Theme.js
+++ scripts/Theme.js
@@ -1,5 +1,14 @@
 function go(){
     elem = document.getElementById('theme');
-    elem.className = (elem.className == 'Background' ? 'newBackground' : 'Background');
+    // elem.className = (elem.className == 'Background' ? 'newBackground' : 'Background');
+    // the code above  rewrite classList, but we need only add
+   if(elem.classList.contains('Background')){
+    elem.classList.remove('Background');
+    elem.classList.add('newBackground');
+   }else{
+    elem.classList.remove('newBackground');
+    elem.classList.add('Background');
+   }
+
     
 }
